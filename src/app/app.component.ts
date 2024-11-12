e6032d926b06d8676630c937d20b27b9b521ece5
Giovanni Silva <git@gsilva.pro>
2024-11-12 19:49:26 -0300

    exemplos de operadores e criacao de observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable, of, Subscription } from 'rxjs';
+import { Observable, of, Subscription, timeInterval, 
+  timer, interval, map, filter, skip, catchError, tap } from 'rxjs';
 import { User } from './model/user';
 import { UserService } from './services/user.service';
 
@@ -25,9 +26,11 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   subscription!: Subscription
-  
-  constructor(private userService: UserService) {}
-  
+
+  intervalo = interval(1000)
+
+  constructor(private userService: UserService) { }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
   }
@@ -35,12 +38,34 @@ export class AppComponent implements OnInit, OnDestroy {
   create() {
     this.userService.create(this.user)
   }
-  
+
   ngOnInit(): void {
     this.users = this.userService.list()
     this.subscription = this.userService.list().subscribe(users => {
       this.usersArray = users
     })
+
+    this.intervalo
+      .pipe(
+        map(valor => valor * 2),
+        skip(3),
+        tap(valor => {
+          if(valor === 12) {
+            throw new Error('Valor === 12')
+          }
+        }),
+        filter(valor => valor <= 20),
+        catchError(error => { 
+          console.error(error);
+          return of(10) 
+        })
+      )
+      .subscribe((valor) => {
+        console.log("Emitindo...")
+        console.log(valor)
+      })
+
+
   }
 
 
